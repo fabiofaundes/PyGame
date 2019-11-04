58ea47653348febf9c6d8d7c044e830b874055b0
Fábio Faúndes <30930267+Fabio-Faundes@users.noreply.github.com>
2019-11-04 11:42:22 -0300

    Jogo funcionando

--- game.py
+++ game.py
@@ -1,8 +1,7 @@
-from tkinter import *
 from enum import Enum
 from math import floor
 from random import randint
-from time import sleep as sleep
+import pygame
 
 class Direction(Enum):
     UP = 0
@@ -40,28 +39,30 @@ class Snake:
         currentX = initialCoordinate.getX()
         for i in range(self.size):
             if i == 0:
-                self.body[i] = initialCoordinate
+                x = initialCoordinate.getX()
+                y = initialCoordinate.getY()
+                self.body[i] = pygame.Rect(x, y, self.seg_size, self.seg_size)
             else:
-                self.body[i] = Coordinate(currentX, initialCoordinate.getY())
+                self.body[i] = pygame.Rect(currentX, initialCoordinate.getY(), self.seg_size, self.seg_size)
             currentX -= self.seg_size
 
-    def incPos(self):        
-        if self.direction == Direction.RIGHT:
-            self.body[0] = Coordinate(self.body[0].getX()+self.seg_size, self.body[0].getY())
-        elif self.direction == Direction.DOWN:
-            self.body[0] = Coordinate(self.body[0].getX(), self.body[0].getY()+self.seg_size)
-        elif self.direction == Direction.LEFT:
-            self.body[0] = Coordinate(self.body[0].getX()-self.seg_size, self.body[0].getY())
-        elif self.direction == Direction.TOP:
-            self.body[0] = Coordinate(self.body[0].getX(), self.body[0].getY()-self.seg_size)
-
+    def incPos(self):                
         for i in range(self.size):
-            if i != 0:
-                self.body[i] = Coordinate(self.body[i-1].getX(), self.body[i-1].getY())
+            index = self.size -1 - i
+            if not index == 0:
+                self.body[index] = self.body[index -1].copy()
+            
+        if self.direction == Direction.RIGHT:
+            self.body[0] = self.body[0].move(self.seg_size, 0)
+        elif self.direction == Direction.DOWN:
+            self.body[0] = self.body[0].move(0, self.seg_size)
+        elif self.direction == Direction.LEFT:
+            self.body[0] = self.body[0].move(-self.seg_size, 0)
+        elif self.direction == Direction.UP:
+            self.body[0] = self.body[0].move(0, -self.seg_size)
 
-    def incSize(self):        
-        lastSeg = self.body[self.size-1]
-        self.body[self.size] = Coordinate(lastSeg.getX(), lastSeg.getY())
+    def incSize(self):                
+        self.body[self.size] = self.body[self.size-1].copy()
         self.incPos()
         self.size += 1
 
@@ -80,30 +81,20 @@ class App:
     WIDTH = 600
     HEIGHT = 600    
     SEG_SIZE = 10 #Tamanho de cada quadrado
-    def __init__(self, master=None):
-        
-        self.wdg1 = Frame(master)
-        self.wdg1.pack(fill='both', expand=True)
-
-        self.wdg1.canvas = Canvas(master, width=self.WIDTH, height=self.HEIGHT, bd=5, bg="black")
-        self.wdg1.canvas.pack()
+    def __init__(self, screen):  
+        self.screen = screen
 
         #iniciação da cobra, definindo a primeira posição como o meio da tela
         self.initialCoordinate = Coordinate(floor(self.WIDTH/2)-self.SEG_SIZE, floor(self.HEIGHT/2)-self.SEG_SIZE)
         self.snake = Snake(10, Direction.RIGHT, self.initialCoordinate, self.SEG_SIZE)
         self.food = None
-        self.generateFood()        
-
-        #desenha tudo na tela
-        self.drawBackground()
-        self.drawSnake()
-        self.drawFood()
+        self.generateFood()                
 
     def generateFood(self):
         while(not self.foodValid()):
             x = randint(1, self.WIDTH/self.SEG_SIZE - 2)
             y = randint(1, self.HEIGHT/self.SEG_SIZE - 2)
-            self.food = Coordinate(x*self.SEG_SIZE, y*self.SEG_SIZE)
+            self.food = pygame.Rect(x*self.SEG_SIZE, y*self.SEG_SIZE, self.SEG_SIZE, self.SEG_SIZE)
 
 
     def foodValid(self):
@@ -117,33 +108,78 @@ class App:
         return True    
 
     def drawSnake(self):
-        for i in range(self.snake.getSize()):
-            x = self.snake.getSegment(i).getX()
-            y = self.snake.getSegment(i).getY()
-            self.wdg1.canvas.create_rectangle(x, y, x+self.SEG_SIZE, y+self.SEG_SIZE, fill='white', outline='white')
+        for i in range(self.snake.getSize()):            
+            pygame.draw.rect(self.screen, (255,255,255), self.snake.getSegment(i))
 
-    def drawFood(self):
-        x = self.food.getX()
-        y = self.food.getY()
-        self.wdg1.canvas.create_rectangle(x, y, x+self.SEG_SIZE, y+self.SEG_SIZE, fill='red', outline='red')
+    def drawFood(self):        
+        pygame.draw.rect(self.screen,(255, 0, 0), self.food)
 
     def drawBackground(self):
-        self.wdg1.canvas.create_rectangle(0,0,self.WIDTH,self.HEIGHT, fill='black', outline='white')              
+        self.screen.fill((0,0,0))
 
-root = Tk()
-root.geometry("600x600")
-root.resizable(0, 0)
-app = App(root)
+    def checkCollision(self):
+        head = self.snake.getSegment(0)
+        for i in range(self.snake.getSize()):
+            if not i == 0:
+                if self.snake.getSegment(i).colliderect(head):
+                    return True
 
-def gameloop():
-    sleep(100)
-    app.snake.incPos()
-    app.drawBackground()
-    app.drawFood()
-    app.drawSnake()
-    root.after(100, gameloop())
+        if(head.left < 0):
+            return True
+        if(head.left > self.WIDTH-self.SEG_SIZE):
+            return True
+        if(head.top < 0):
+            return True
+        if(head.top > self.HEIGHT-self.SEG_SIZE):
+            return True
 
-root.after(1000, gameloop())
-root.mainloop()
+        return False
 
-    
+    def checkFood(self):
+        head = self.snake.getSegment(0)
+        if head.colliderect(self.food):
+            return True
+
+
+pygame.init()
+screen = pygame.display.set_mode((600,600))
+game = App(screen)
+points = 0
+
+clock = pygame.time.Clock()
+
+winn = False
+over = False
+
+while not winn and not over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            over = True
+
+    key_pressed = pygame.key.get_pressed()
+    if key_pressed[pygame.K_UP]:
+        if not game.snake.direction == Direction.DOWN:
+            game.snake.changeDir(Direction.UP)
+    elif key_pressed[pygame.K_DOWN]:
+        if not game.snake.direction == Direction.UP:
+            game.snake.changeDir(Direction.DOWN)
+    elif key_pressed[pygame.K_RIGHT]:
+        if not game.snake.direction == Direction.LEFT:
+            game.snake.changeDir(Direction.RIGHT)
+    elif key_pressed[pygame.K_LEFT]:
+        if not game.snake.direction == Direction.RIGHT:
+            game.snake.changeDir(Direction.LEFT)
+
+    game.snake.incPos()
+    game.drawBackground()
+    game.drawFood()
+    game.drawSnake()
+    pygame.display.flip()
+
+    over = game.checkCollision()        
+    if game.checkFood():
+        game.snake.incSize()
+        game.generateFood()
+        points += 1
+
+    clock.tick(15)
